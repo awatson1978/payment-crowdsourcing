14871d3bb8975171d15c9fd1d60f6b1cc8a82941
Abigail Watson <awatson1978@gmail.com>
2015-07-07 17:54:13 -0500

    updates

--- webapp/client/stripe.js
+++ webapp/client/stripe.js
@@ -1,12 +1,5 @@
 // subscription to Stripe
 
-// VONVO PUBLISHABLE KEY: pk_test_HVcX0jn1NPjRyhR8hyzKbcjG
-//Stripe.setPublishableKey('pk_test_HVcX0jn1NPjRyhR8hyzKbcjG');
-
-
 // PENTASYLLABIC PUBLISHABLE_KEY:  pk_test_jnn4SomhH02WRflSNoe6MPws
 // test
 Stripe.setPublishableKey('pk_test_jnn4SomhH02WRflSNoe6MPws');
-
-// live
-//Stripe.setPublishableKey('pk_live_J90OUdTdWJVAOPy3iOsuGWUd');
